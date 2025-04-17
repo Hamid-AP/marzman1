c618a8069ce8202428be92176fe21d6a3c286492
Hamid-AP <h.heydaripoor3433@gmail.com>
2025-04-17 22:54:22 +0330

    Update app.js
    Update

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const select = document.getElementById("friend-select");
+const image = document.getElementById("friend-image");
+const quote = document.getElementById("quote");
 
-{
-  "name": "مرز من",
-  "short_name": "مرز من",
-  "start_url": ".",
-  "display": "standalone",
-  "background_color": "#ffffff",
-  "theme_color": "#2b7a78",
-  "icons": []
-}
+const quotes = [
+  "تو خیلی باارزشی، حتی اگه دیگران نفهمن!",
+  "اشکالی نداره گاهی ناراحت باشی، قوی بودن یعنی ادامه دادن.",
+  "مرز داشتن نشونه احترام به خودته.",
+  "تو حق داری بگی «نه» وقتی راحت نیستی."
+];
+
+select.addEventListener("change", () => {
+  image.src = select.value;
+});
+
+quote.innerText = quotes[Math.floor(Math.random() * quotes.length)];
