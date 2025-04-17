b4434e832143411ba774ce6c29b8a8edee823b09
Hamid-AP <h.heydaripoor3433@gmail.com>
2025-04-17 22:53:21 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+{
+  "name": "مرز من",
+  "short_name": "مرز من",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#2b7a78",
+  "icons": []
+}
